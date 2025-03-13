a07f4d2335f8a6a0efcded81fbc4d9e602abfc1d
ngocvo2511 <ngocvo12345ssssaaaa@gmail.com>
2025-03-13 18:35:35 +0700

    Add MainViewModel and resource dictionary integration
    - Modified App.xaml to include a ResourceDictionary in Application.Resources, merging ResourceXAML/MainResource.xaml.
    - Updated MainWindow.xaml to set DataContext to static resource MainVM, binding to MainViewModel.
    - Added constructor to MainViewModel.cs with MessageBox.Show call on initialization.
    - Created MainResource.xaml defining ResourceDictionary with MainViewModel as MainVM.

--- AirTicketSalesManagement/ViewModel/MainViewModel.cs
+++ AirTicketSalesManagement/ViewModel/MainViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AirTicketSalesManagement.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        public MainViewModel()
+        {
+            // Initialize the view model
+            MessageBox.Show("MainViewModel initialized");
+        }
     }
 }
