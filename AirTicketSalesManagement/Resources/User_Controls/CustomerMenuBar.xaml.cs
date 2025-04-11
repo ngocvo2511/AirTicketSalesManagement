81a93782b3670eef5690dd01c856f0467510c6bb
nhn141 <142210395+nhn141@users.noreply.github.com>
2025-04-11 15:45:04 +0700

    Update Đăng nhập Khách hàng: trang chủ, đặt vé, thông tin cá nhân
    Update Đăng nhập
    Khách hàng: trang chủ, đặt vé, thông tin cá nhân

---
+++ AirTicketSalesManagement/Resources/User_Controls/CustomerMenuBar.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AirTicketSalesManagement.ViewModel;
+
+namespace AirTicketSalesManagement.Resources.User_Controls
+{
+    /// <summary>
+    /// Interaction logic for CustomerMenuBar.xaml
+    /// </summary>
+    public partial class CustomerMenuBar : UserControl
+    {
+        public CustomerMenuBar()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel.MenuBarViewModel();
+        }
+    }
+}
