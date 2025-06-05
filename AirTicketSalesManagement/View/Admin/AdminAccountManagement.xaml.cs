fcc6f701cd86483ba0d0bfcd97607995ea5edf3a
ChuTungDuongg <tungduong156gli@gmail.com>
2025-06-06 01:07:31 +0700

    Them giao dien phan xoa, sua, them tai khoan trong muc quan ly tai khoan

---
+++ AirTicketSalesManagement/View/Admin/AdminAccountManagement.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AirTicketSalesManagement.View.Admin
+{
+    public partial class AdminAccountManagement : UserControl
+    {
+        public AdminAccountManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
