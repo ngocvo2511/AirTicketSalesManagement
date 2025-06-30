0ab427cda48841a47cfb1caecbadcbcc2dada410
nhn141 <142210395+nhn141@users.noreply.github.com>
2025-06-30 21:02:54 +0700

    update notification view model

--- AirTicketSalesManagement/ViewModel/NotificationViewModel.cs
+++ AirTicketSalesManagement/ViewModel/NotificationViewModel.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Media;
 
 namespace AirTicketSalesManagement.ViewModel
 {
-    class NotificationViewModel
+    public enum NotificationType
     {
+        Information,
+        Warning,
+        Error
+    }
+
+    public partial class NotificationViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title;
+
+        [ObservableProperty]
+        private string message;
+
+        [ObservableProperty]
+        private bool isVisible;
+
+        [ObservableProperty]
+        private Brush background;
+
+        public NotificationViewModel(){}
+
+        [RelayCommand]
+        private void Close()
+        {
+            IsVisible = false;
+        }
+
+        public void ShowNotification(string message, NotificationType type)
+        {
+            Message = message;
+            IsVisible = true;
+
+            switch (type)
+            {
+                case NotificationType.Information:
+                    Title = "Thông báo";
+                    Background = Brushes.DodgerBlue;
+                    break;
+                case NotificationType.Warning:
+                    Title = "Cảnh báo";
+                    Background = Brushes.Orange;
+                    break;
+                case NotificationType.Error:
+                    Title = "Lỗi";
+                    Background = Brushes.Crimson;
+                    break;
+            }
+        }
     }
 }
